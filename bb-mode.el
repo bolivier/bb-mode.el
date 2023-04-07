1d569faf9fa99ba4f2b968c1a5138a5fcdebdd20
Brandon Olivier <brandon@brandonolivier.com>
2023-04-07 11:51:59 -0500

    Start task selection setup

--- bb-mode.el
+++ bb-mode.el
@@ -9,7 +9,7 @@
 ;; Version: 0.0.1
 ;; Keywords: abbrev bib c calendar comm convenience data docs emulations extensions faces files frames games hardware help hypermedia i18n internal languages lisp local maint mail matching mouse multimedia news outlines processes terminals tex tools unix vc wp
 ;; Homepage: https://github.com/brandon/bb-mode
-;; Package-Requires: ((emacs "24.3"))
+;; Package-Requires: ((emacs "24.4"))
 ;;
 ;; This file is not part of GNU Emacs.
 ;;
@@ -42,6 +42,7 @@
 (defvar bb-mode--command-map
   (let ((map (make-sparse-keymap)))
     (define-key map "h" 'bb-mode-greet)
+    (define-key map "t" 'bb-mode-greet)
     map)
   "Keymap for `bb-mode' commands")
 
@@ -50,6 +51,18 @@
     (define-key map (kbd bb-mode--command-prefix) bb-mode--command-map)
     map)
   "Keymap for `bb-mode'")
+
+(defun bb-mode--get-bb-tasks ()
+  (interactive)
+  (let* ((dir (locate-dominating-file default-directory "bb.edn"))
+         (filename (concat dir "bb.edn")))
+    (with-file-contents! filename
+      (let ((bb-contents (car (parseedn-read))))
+        (-remove
+         #'keywordp
+         (hash-table-keys (gethash
+                           :tasks
+                           bb-contents)))))))
 
 (define-minor-mode bb-mode
   "Mode for working with bb.edn tasks within Emacs"
